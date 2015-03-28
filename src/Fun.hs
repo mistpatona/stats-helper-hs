c15a8b905eb6ac6f6e8d569eb45b69c576792451
Serge Y. Petrov <mist.patona@gmail.com>
2015-03-28 15:09:43 +0200

    add test for choose, add choose, generalize Int to Integral

--- src/Fun.hs
+++ src/Fun.hs
@@ -2,7 +2,16 @@ module Fun where
 
 down :: (Integral a) => a -> a -> a
 down n j = foldr1 (*) [n-j+1..n] 
-factorial :: Int -> Int
+{-# SPECIALISE down :: Int->Int->Int     #-}
+{-# SPECIALISE down :: Integer->Integer->Integer #-}
+
+factorial :: (Integral a) => a -> a
 factorial 0 = 1
 factorial n = foldr1 (*) [1..n]
+{-# SPECIALISE factorial :: Int->Int     #-}
+{-# SPECIALISE factorial :: Integer->Integer #-}
 
+choose :: (Integral a) => a -> a -> a
+n `choose` k = (n `down` k) `div` factorial k
+{-# SPECIALISE choose :: Int->Int->Int     #-}
+{-# SPECIALISE choose :: Integer->Integer->Integer #-}
--- src/Fun_Test.hs
+++ src/Fun_Test.hs
@@ -1,8 +1,12 @@
 import Fun
+
 import Test.HUnit
 
 testFactorial1 = TestCase $ assertEqual "4! = 24" 24 (factorial 4)
 
 testDown1 = TestCase $ assertEqual "5 down 2 = 5*4 = 20" 20 ( 5 `down` 2)
 
-main = runTestTT $ TestList [testFactorial1, testDown1]
+testChoose1 = TestCase $ assertEqual "9 choose 2 = 36" 36 (9 `choose` 2)
+
+main = runTestTT $ TestList [testFactorial1, testDown1, testChoose1]
+
